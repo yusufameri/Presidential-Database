f5c62cd9f364cea7dc2a875df09e22e01694457a
Douglas Sexton <Douglas Sexton>
2016-12-03 18:00:49 -0500

    Created website using bootstrap and updated candidate query page so drop down retrieves candidates from database

--- presidentialSelections/candidate.php
+++ presidentialSelections/candidate.php
@@ -5,6 +5,12 @@ if (isset($_POST['action'])) {
         case 'get_candidates': // Only one case right now
             get_candidates();
             break;
+		case 'build_candidate_selection_box':
+			build_candidate_selection_box();
+			break;
+		case 'candidate_query':
+			candidate_query($_POST['data']);
+			break;
     }
 }
 
@@ -39,4 +45,61 @@ function get_candidates() {
 	}
 	echo "</table>";
 }
+
+function build_candidate_selection_box() {
+	// The echoed statements get returned to the html
+	$user = 'root';
+	$pass = '';
+	$db = 'presidential_elections';
+
+	$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to database");
+
+	$sql = "SELECT * FROM `candidate` WHERE 1";
+	$result = $db->query($sql);
+	
+	echo "<select id='select_box_id'>";
+	
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<option value='" . $row['name'] . "'>" . $row['name'] ."</option>";
+		}
+	} else {
+			echo "<option value='" . "DB_ERROR" . "'>" . "DB Access FAILED" ."</option>";
+	}
+	echo "</select>";
+}
+
+function candidate_query($candidate) {
+	// The echoed statements get returned to the html
+	$user = 'root';
+	$pass = '';
+	$db = 'presidential_elections';
+
+	$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to database");
+
+	$sql = "SELECT * FROM candidate WHERE candidate.name=\"" . $candidate . "\"";
+	$result = $db->query($sql);
+	
+	echo "<table border='1'>
+	<tr>
+	<th>Name</th>
+	</tr>";
+	echo "<tr>";
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>" . $row['name'] . "</td>";
+			echo "</tr>";
+		}
+	} else {
+			echo "<tr>";
+			echo "<td>" . "0 Results" . "</td>";
+			echo "</tr>";
+	}
+	echo "</table>";
+}
+
 ?>
